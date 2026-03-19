d29a23951b297f30229cf4a9a02e3bbc9726a34a
Arvin Rapstad <101880810+amrapstad@users.noreply.github.com>
2026-03-19 01:01:44 +0100

    Divided the components and made the frontend a little cleaner. Moved from jsx to tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { useState } from 'react'
+import * as React from 'react';
+import Search from './sites/Search'
+
+function App() {
+  return (
+    <>
+      <Search />
+    </>
+  )
+}
+
+
+export default App
